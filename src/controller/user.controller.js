3e213b526a74794b99dd09e5297b5ae1727522f3
Afowowe Olumide Victor <volumide42@gmail.com>
2023-09-06 11:50:08 +0100

    implement login logic

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,7 +1,9 @@
+import { compare } from "bcrypt"
 import User from "../models/user.model"
-import { SERVER_ERROR, CREATED } from "../utils/status-codes.js"
+import { SERVER_ERROR, CREATED, BAD_REQUEST, SUCCESS } from "../utils/status-codes.js"
+import { accessToken } from "../utils/middleware"
 
-const signup = async (req, res) => {
+export const signup = async (req, res) => {
   try {
     const { email, password, first_name, last_name, phone } = req.body
     const reg = await User.create({
@@ -14,6 +16,46 @@ const signup = async (req, res) => {
     return res.status(CREATED).json({
       status: CREATED,
       message: "sign up success"
+    })
+  } catch (error) {
+    return res.status(SERVER_ERROR).json({
+      status: SERVER_ERROR,
+      message: "server error"
+    })
+  }
+}
+
+export const login = async (req, res) => {
+  try {
+    const { email, password: pss } = req.body
+    const user = await User.findOne({ where: { email: email } })
+
+    if (!user) {
+      return res.status(NOT_FOUND).json({
+        status: NOT_FOUND,
+        message: "user not found"
+      })
+    }
+
+    const verifyPassword = compare(pss, user.password)
+
+    if (!verifyPassword) {
+      return res.status(BAD_REQUEST).json({
+        status: BAD_REQUEST,
+        message: "invalid password"
+      })
+    }
+
+    const {
+      dataValues: { password, ...data }
+    } = user
+
+    const token = accessToken(data)
+    return res.status(SUCCESS).json({
+      status: SUCCESS,
+      token,
+      body: data,
+      message: ""
     })
   } catch (error) {
     return res.status(SERVER_ERROR).json({
